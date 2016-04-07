163815c36c680fdb1690f8f022a377c137308288
szydan <danielczyk.szymon@gmail.com>
2016-04-08 00:19:36 +0100

    added proper support of nested fields

---
+++ public/lib/helpers/timeline_helper.js
@@ -0,0 +1,32 @@
+define(function (require) {
+  return function TimelineHelperFactory() {
+    var _ = require('lodash');
+
+    function TimelineHelper() {
+    }
+
+    TimelineHelper.prototype.getDescendantPropValue = function (obj, propertyPath) {
+      var arr = propertyPath.split('.');
+      while (arr.length && (obj = obj[arr.shift()]));
+      return obj;
+    };
+
+    TimelineHelper.prototype.isMultivalued = function (value) {
+      return value instanceof Array;
+    };
+
+    TimelineHelper.prototype.pickFirstIfMultivalued  = function (value, defaultValue) {
+      if (!value) {
+        return defaultValue || '';
+      } else {
+        if (value instanceof Array && value.length > 0) {
+          return value[0];
+        } else {
+          return value;
+        }
+      }
+    };
+
+    return new TimelineHelper();
+  };
+});
