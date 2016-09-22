f7a2ba6d3a69e6d6575d26da8aaf1d7b66ae04cf
szydan <danielczyk.szymon@gmail.com>
2016-09-22 15:07:53 +0100

    fixed the controller test

--- public/__tests__/kibi_timeline_vis_controller.js
+++ public/__tests__/kibi_timeline_vis_controller.js
@@ -1,11 +1,14 @@
-describe('Kibi Controllers', function () {
-  var $scope;
-  var ngMock = require('ngMock');
-  var expect = require('expect.js');
+var ngMock = require('ngMock');
+var expect = require('expect.js');
+var $ = require('jquery');
+require('../kibi_timeline_vis_controller');
 
-  require('../kibi_timeline_vis_controller');
+describe('Kibi Timeline', function () {
+  var $scope;
+  var $element;
 
   function init(options) {
+
     ngMock.module('kibana', function ($provide) {
       $provide.constant('kbnDefaultAppId', '');
       $provide.constant('kibiDefaultDashboardId', '');
@@ -13,22 +16,46 @@ describe('Kibi Controllers', function () {
       $provide.constant('elasticsearchPlugins', ['siren-join']);
     });
 
-    ngMock.inject(function ($rootScope, $controller) {
-      $scope = $rootScope;
-      var $element = '<div></div>';
+    ngMock.inject(function (_$rootScope_, $controller) {
+      var fakeRoute = {
+        current: {
+          locals: {
+          }
+        }
+      };
 
-      $controller('KbnTimelineVisController', { $scope:$scope, $element:$element });
+      $scope = _$rootScope_;
+      $scope.vis = {
+        id: 'a'
+      };
+      $element = $('<div></div>');
+      $controller('KbnTimelineVisController', {
+        $scope: $scope,
+        $route: fakeRoute,
+        $element: $element
+      });
       $scope.$digest();
     });
   }
 
-  describe('Kibi Timeline Visualization controller', function () {
-    it('should not render templates if no query is set', function () {
-      var params = {};
+  describe('Controller', function () {
 
-      init({ params: params });
-      $scope.renderTemplates();
-      expect($scope).to.be.ok();
+    it('Should set the correct options when change:vis event is triggered', function () {
+      init();
+      expect($scope.options).not.to.be.ok();
+
+      $element.offsetHeight = 20;
+      $scope.$emit('change:vis');
+
+      var expectedOptions = {
+        width: '100%',
+        height: '175px',
+        selectable: true,
+        autoResize: false
+      };
+
+      expect($scope.options).to.eql(expectedOptions);
     });
+
   });
 });
