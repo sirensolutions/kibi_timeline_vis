ee30902567f8ce6aa668354c4544e17b46025cbc
Stéphane Campinas <stephane.campinas@gmail.com>
2016-11-09 16:39:50 +0000

    check for daylight saving time changes

--- public/lib/helpers/__tests__/timeline_helper.js
+++ public/lib/helpers/__tests__/timeline_helper.js
@@ -1,105 +1,119 @@
-var expect = require('expect.js');
-var timelineHelper = require('../timeline_helper')();
+const expect = require('expect.js');
+const timelineHelper = require('../timeline_helper')();
+const moment = require('moment');
 
-describe('TimelineHelper', function () {
+describe('Kibi Timeline', function () {
+  describe('TimelineHelper', function () {
+    describe('getDescendantPropValue', function () {
+      it('no data', function () {
+        var o = {};
+        expect(timelineHelper.getDescendantPropValue(o, 'p1.p2')).to.be(undefined);
+      });
 
-  describe('getDescendantPropValue', function () {
-    it('no data', function () {
-      var o = {};
-      expect(timelineHelper.getDescendantPropValue(o, 'p1.p2')).to.be(undefined);
-    });
-
-    it('null', function () {
-      var o = {
-        p1: {
-          p2: null
-        }
-      };
-      expect(timelineHelper.getDescendantPropValue(o, 'p1.p2')).to.be(null);
-    });
-
-    it('empty string', function () {
-      var o = {
-        p1: {
-          p2: ''
-        }
-      };
-      expect(timelineHelper.getDescendantPropValue(o, 'p1.p2')).to.be('');
-    });
-
-    it('one level', function () {
-      var o = {
-        p1: {
-          p2: 7
-        }
-      };
-      expect(timelineHelper.getDescendantPropValue(o, 'p1.p2')).to.be(7);
-    });
-
-    it('one level array', function () {
-      var o = {
-        p1: {
-          p2: [7]
-        }
-      };
-      expect(timelineHelper.getDescendantPropValue(o, 'p1.p2')).to.eql([7]);
-    });
-
-
-    it('two levels', function () {
-      var o = {
-        p1: {
-          p2: {
-            p3: 7
+      it('null', function () {
+        var o = {
+          p1: {
+            p2: null
           }
-        }
-      };
-      expect(timelineHelper.getDescendantPropValue(o, 'p1.p2.p3')).to.be(7);
-    });
+        };
+        expect(timelineHelper.getDescendantPropValue(o, 'p1.p2')).to.be(null);
+      });
 
-    it('two levels array', function () {
-      var o = {
-        p1: {
-          p2: {
-            p3: [7]
+      it('empty string', function () {
+        var o = {
+          p1: {
+            p2: ''
           }
+        };
+        expect(timelineHelper.getDescendantPropValue(o, 'p1.p2')).to.be('');
+      });
+
+      it('one level', function () {
+        var o = {
+          p1: {
+            p2: 7
+          }
+        };
+        expect(timelineHelper.getDescendantPropValue(o, 'p1.p2')).to.be(7);
+      });
+
+      it('one level array', function () {
+        var o = {
+          p1: {
+            p2: [7]
+          }
+        };
+        expect(timelineHelper.getDescendantPropValue(o, 'p1.p2')).to.eql([7]);
+      });
+
+
+      it('two levels', function () {
+        var o = {
+          p1: {
+            p2: {
+              p3: 7
+            }
+          }
+        };
+        expect(timelineHelper.getDescendantPropValue(o, 'p1.p2.p3')).to.be(7);
+      });
+
+      it('two levels array', function () {
+        var o = {
+          p1: {
+            p2: {
+              p3: [7]
+            }
+          }
+        };
+        expect(timelineHelper.getDescendantPropValue(o, 'p1.p2.p3')).to.eql([7]);
+      });
+    });
+
+    describe('changeTimezone', function () {
+      it('should return Browser for default Kibana timezone ', function () {
+        expect(timelineHelper.changeTimezone('Browser')).to.be('Browser');
+      });
+
+      it('should return -04:00 for America/Nassau timezone ', function () {
+        if (moment('America/Nassau').isDST()) {
+          expect(timelineHelper.changeTimezone('America/Nassau')).to.be('-04:00');
+        } else {
+          expect(timelineHelper.changeTimezone('America/Nassau')).to.be('-05:00');
         }
-      };
-      expect(timelineHelper.getDescendantPropValue(o, 'p1.p2.p3')).to.eql([7]);
-    });
-  });
+      });
 
-  describe('changeTimezone', function () {
-    it('should return Browser for default Kibana timezone ', function () {
-      expect(timelineHelper.changeTimezone('Browser')).to.be('Browser');
-    });
+      it('should return 13 for Etc/GMT-13 timezone ', function () {
+        expect(timelineHelper.changeTimezone('Etc/GMT-13')).to.be('+13:00');
+      });
 
-    it('should return -04:00 for America/Nassau timezone ', function () {
-      expect(timelineHelper.changeTimezone('America/Nassau')).to.be('-04:00');
-    });
+      it('should return -4 for Etc/GMT+4 timezone ', function () {
+        expect(timelineHelper.changeTimezone('Etc/GMT+4')).to.be('-04:00');
+      });
 
-    it('should return 13 for Etc/GMT-13 timezone ', function () {
-      expect(timelineHelper.changeTimezone('Etc/GMT-13')).to.be('+13:00');
-    });
+      it('should return 0 for Etc/GMT timezone ', function () {
+        expect(timelineHelper.changeTimezone('Etc/GMT')).to.be('+00:00');
+      });
 
-    it('should return -4 for Etc/GMT+4 timezone ', function () {
-      expect(timelineHelper.changeTimezone('Etc/GMT+4')).to.be('-04:00');
-    });
+      it('should return 0 for Etc/GMT0 timezone ', function () {
+        expect(timelineHelper.changeTimezone('Etc/GMT0')).to.be('+00:00');
+      });
 
-    it('should return 0 for Etc/GMT timezone ', function () {
-      expect(timelineHelper.changeTimezone('Etc/GMT')).to.be('+00:00');
-    });
+      it('should return +09:30 for Australia/Darwin timezone ', function () {
+        if (moment('Australia/Darwin').isDST()) {
+          expect(timelineHelper.changeTimezone('Australia/Darwin')).to.be('+08:30');
+        } else {
+          expect(timelineHelper.changeTimezone('Australia/Darwin')).to.be('+09:30');
+        }
+      });
 
-    it('should return 0 for Etc/GMT0 timezone ', function () {
-      expect(timelineHelper.changeTimezone('Etc/GMT0')).to.be('+00:00');
-    });
-
-    it('should return +09:30 for Australia/Darwin timezone ', function () {
-      expect(timelineHelper.changeTimezone('Australia/Darwin')).to.be('+09:30');
-    });
-
-    it('should return +05:45 for Asia/Katmandu timezone ', function () {
-      expect(timelineHelper.changeTimezone('Asia/Katmandu')).to.be('+05:45');
+      it('should return +05:45 for Asia/Katmandu timezone ', function () {
+        if (moment('Asia/Katmandu').isDST()) {
+          expect(timelineHelper.changeTimezone('Asia/Katmandu')).to.be('+04:45');
+        } else {
+          expect(timelineHelper.changeTimezone('Asia/Katmandu')).to.be('+05:45');
+        }
+      });
     });
   });
 });
