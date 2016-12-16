5566c8121561055297cffe83b392b2a5c27545d0
SergeyBondarenko <sergibondarenko@gmail.com>
2016-12-16 09:42:01 +0100

    Delete unused method.

--- public/lib/helpers/timeline_helper.js
+++ public/lib/helpers/timeline_helper.js
@@ -11,18 +11,6 @@ define(function (require) {
       return value instanceof Array && value.length > 1;
     };
 
-    TimelineHelper.prototype.pickFirstIfMultivalued  = function (value, defaultValue) {
-      if (!value) {
-        return defaultValue || '';
-      } else {
-        if (value instanceof Array && value.length > 0) {
-          return value[0];
-        } else {
-          return value;
-        }
-      }
-    };
-
     TimelineHelper.prototype.changeTimezone  = function (timezone) {
       if (timezone !== 'Browser') {
         return moment().tz(timezone).format('Z');
