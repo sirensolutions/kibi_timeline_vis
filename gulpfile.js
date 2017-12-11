ca28aa06265c529c6c25bc5993f230f9316b4b0b
Szymon Danielczyk <danielczyk.szymon@gmail.com>
2017-12-11 10:40:21 +0000

    removed version from package name

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var spawn = require('child_process').spawn;
 var minimist = require('minimist');
 
 var pkg = require('./package.json');
-var packageName = pkg.name  + '-' + pkg.version;
+var packageName = pkg.name;
 
 var buildDir = path.resolve(__dirname, 'build/gulp');
 var targetDir = path.resolve(__dirname, 'target/gulp');
@@ -30,7 +30,7 @@ var knownOptions = {
 };
 var options = minimist(process.argv.slice(2), knownOptions);
 
-var kibanaPluginDir = path.resolve(__dirname, options.kibanahomepath + '/plugins/' + pkg.name);
+var kibanaPluginDir = path.resolve(__dirname, options.kibanahomepath + '/plugins/' + packageName);
 
 
 function syncPluginTo(dest, done) {
