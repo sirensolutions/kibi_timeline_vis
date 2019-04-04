e1c24d38d10ab181df80e019e966c2048b1220e7
szydan <danielczyk.szymon@gmail.com>
2019-04-04 12:07:57 +0100

    Add dependency to migrations plugin

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function (kibana) {
   return new kibana.Plugin({
     name: 'kibi_timeline',
-    require: ['kibana', 'elasticsearch'],
+    require: ['kibana', 'elasticsearch', 'migrations'],
     uiExports: {
       visTypes: [
         'plugins/kibi_timeline_vis/kibi_timeline_vis'
